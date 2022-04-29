b9b7c1b9142617d49c3f7a394e3e874eb3faa1c4
Btaype <90937585+btaype@users.noreply.github.com>
2022-04-29 07:14:13 +0200

    Add files via upload

---
+++ Descifrado_fuerza.py
@@ -0,0 +1,64 @@
+
+def e_euclides(a,b): 
+    if (b == 0):
+      return a,1,0
+    d,x1,y1 = e_euclides(b, a % b)
+    x = y1
+    y = x1 - y1 * int(a / b)
+    return d,x,y
+
+def euclides(a, b):
+ if b == 0:
+  return a
+ else:
+  return euclides(b, a % b)
+
+def inverso(a,n):
+  if (euclides(a,n)==1):
+    d,x,y=e_euclides(a,n)
+    return(x%n)
+  else:
+    return "no tiene inverso por la ptmre:"
+
+def cifrar(a,b,cadena):
+
+  cadena2=""
+  abc="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+
+  for i in cadena:
+    n=0
+    for j in abc:
+      if i==j:
+        break
+      n=n+1 
+    y=(a * n + b )% 27
+    cadena2=cadena2+abc[y]
+
+  return cadena2
+
+def decifrar(a,b,cadena):
+  cadena2=""
+  abc="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+
+  for i in cadena:
+    n=0
+    for j in abc:
+      if i==j:
+        break
+      n=n+1 
+    x=(inverso(a,27)*(n-b)) % 27
+    cadena2=cadena2+abc[x]
+  return cadena2
+
+mensaje="SLBCMVRBSHZBTÑSRQVVMSZBVHÑBVRQVLALHZBTÑSRQVWQAXLZWÑAQFQV"
+
+for a in range(27):
+  if (a%3!=0):
+    for b in range(27):
+      print("[",a,",",b,"]",decifrar(a,b,mensaje))
+      print("\n")
+
+print("respuesta: [",23,",",17, "]",decifrar(23,17,mensaje))
+    
+        
+  
